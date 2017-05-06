19e3dbf891a6ed440d8bf964b4580b5e3e64d6e1
Marilena Alexe <marilena.alexe@gmail.com>
2017-05-06 11:19:06 +0100

    Create initialize(), save() and delete_all() methods for airline class and feed seeds.rb with airline data

--- flight_route_tracker/models/airline.rb
+++ flight_route_tracker/models/airline.rb
@@ -1,3 +1,5 @@
+require('pg')
+
 class Airline
 
   attr_reader :id
@@ -7,6 +9,20 @@ class Airline
     @id = params['id'].to_i if params['id']
     @airline_name = params['airline_name']
     @logo = params['logo']
+  end
+
+  def save()
+    db = PG.connect({dbname: 'flight_tracker', host: 'localhost'})
+    sql = "INSERT INTO airlines (airline_name,logo) VALUES ('#{@airline_name}','#{@logo}') RETURNING id;"
+    db.exec(sql)
+    db.close()
+  end
+
+  def Airline.delete_all()
+    db = PG.connect({dbname: 'flight_tracker', host: 'localhost'})
+    sql = "DELETE FROM airlines;"
+    db.exec(sql)
+    db.close()
   end
 
 end
