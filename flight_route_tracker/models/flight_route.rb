704dacb3c04fc0ed66f0e801acd0e4a44891c9b9
Marilena Alexe <marilena.alexe@gmail.com>
2017-05-08 00:03:36 +0100

    Update model <flight_route.rb> with more methods

--- flight_route_tracker/models/flight_route.rb
+++ flight_route_tracker/models/flight_route.rb
@@ -13,13 +13,40 @@ class FlightRoute
     @price = params['price']
     @currency = params['currency']
   end
+# -------------Instance-methods--------------
 
-  def save
+  def save()
     sql ="INSERT INTO flight_routes (origin, destination, route, price, currency) VALUES ('#{@origin}', '#{@destination}', '#{@route}', #{price},'#{@currency}') RETURNING id;"
     array_hashes = SqlRunner.run(sql)
     flight_route_hash = array_hashes.first()
     @id = flight_route_hash['id'].to_i
   end
+
+  def airline()
+    sql = "SELECT a.* FROM airlines a INNER JOIN airline_routes ar ON a.id = ar.airline_id INNER JOIN flight_routes fr ON ar.route_id = fr.id WHERE fr.id = #{@id};"
+    array_hashes = SqlRunner.run(sql)
+    result = array_hashes.map {|hash| Airline.new(hash)}
+    return result
+  end
+
+  def deal()
+    sql = "SELECT d.* FROM deals d INNER JOIN route_deals rd ON d.id = rd.deal_id INNER JOIN flight_routes fr ON rd.route_id = fr.id WHERE fr.id = #{@id};"
+    array_hashes = SqlRunner.run(sql)
+    result = array_hashes.map {|hash| Deal.new(hash)}
+    return result
+  end
+
+  def update()
+    sql = "UPDATE flight_routes SET (origin, destination, route, price, currency) = ('#{@origin}', '#{@destination}', '#{@route}', #{@price}, '#{@currency}') WHERE id = #{@id};"
+    SqlRunner.run(sql)
+  end
+
+  def delete()
+    sql = "DELETE FROM flight_routes WHERE id = #{@id};"
+    SqlRunner.run(sql)
+  end
+
+# -------------Class-methods--------------
 
   def FlightRoute.delete_all()
     sql = "DELETE FROM flight_routes;"
@@ -30,6 +57,13 @@ class FlightRoute
     sql = "SELECT * FROM flight_routes;"
     array_hashes = SqlRunner.run(sql)
     result = array_hashes.map {|hash| FlightRoute.new(hash)}
+    return result
+  end
+
+  def FlightRoute.find(id)
+    sql = "SELECT * FROM flight_routes WHERE id = #{id};"
+    route_hash = SqlRunner.run(sql).first()
+    result = FlightRoute.new(route_hash)
     return result
   end
 
